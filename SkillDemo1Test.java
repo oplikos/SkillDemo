5ef13c8e25f3d50b05324ed3b645b1cc094a29a6
sarkis bouzikian <44330982+oplikos@users.noreply.github.com>
2022-02-04 00:55:12 -0800

    edditing the Skill Demo and fixing the assert

--- SkillDemo1Test.java
+++ SkillDemo1Test.java
@@ -5,7 +5,7 @@ public class SkillDemo1Test {
     @Test 
     public void powerTest(){
         //2^4 = 16 
-        assertEquals(32, SkillDemo1.power(2, 4));
+        assertEquals(16, SkillDemo1.power(2, 4));
     }
     
 }
